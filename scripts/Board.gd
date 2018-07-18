87f8c1d96ad05d46bfdb49f9624b75c65f3f93b8
Alvin Ho <ho.alvin857@gmail.com>
2018-07-18 02:24:32 -0700

    Added tic-tac-toe formation
    Added tic tac toe formation and testing glow effects

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -4,12 +4,15 @@ var player1 = 0.0
 var player2 = 0.0
 
 func _ready():
-	
+	var i = 1
+	for pair in get_node("Game").get_children():
+		pair.init(String(i))
+		i += 1
 	pass
 
 func _process(delta):
-	for n in get_node("Game").get_children():
-		var update = n.update_and_get_score()
+	for pair in get_node("Game").get_children():
+		var update = pair.update_and_get_score()
 		player1 += update[2]
 		player2 += update[1] 
 	get_node("Player1_Score").text = String(player1).pad_decimals(1)
--- scripts/RPS_pair.gd
+++ scripts/RPS_pair.gd
@@ -3,15 +3,14 @@ extends Node
 var points_rate = 10
 var stored_points = [0,0,0]
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+func init(pair_number):
+	get_node("Hand1").trigger = "player1_hand"+pair_number
+	get_node("Hand2").trigger = "player2_hand"+pair_number
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-	var winner = get_node("RPS").versus(get_node("RPS2"))
+	var winner = get_node("Hand1").versus(get_node("Hand2"))
 	stored_points[winner] += delta * points_rate
 	pass
 
